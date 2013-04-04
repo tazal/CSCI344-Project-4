815bb0f6687a8029530eabedeab3d14e397e3ee0
Sara Van Buskirk <svanbusk@unca.edu>
2013-04-04 17:54:14 -0400

    Get application to track happy and sad words, and return the number of each as a redis key.

--- twitterworker.js
+++ twitterworker.js
@@ -1,30 +1,43 @@
 var worker = function () {
-    var twitter = require("ntwitter");
-    var redis = require("redis");
-    var credentials = require("./credentials.js");
+  "use strict";
+  var twitter = require("ntwitter");
+  var redis = require("redis");
+  var credentials = require("./credentials.js");
 
-    //create redis client                                                                                                                                                                                                                       
-    var client = redis.createClient();
+  //create redis client                                                                                                                                                                                                                       
+  var client = redis.createClient(),
+    t = new twitter({
+      consumer_key: credentials.consumer_key,
+      consumer_secret: credentials.consumer_secret,
+      access_token_key: credentials.access_token_key,
+      access_token_secret: credentials.access_token_secret
+    }),
+    happyArray = ["happy"],
+    sadArray = ["sad"],
+    trackedArray = happyArray.concat(sadArray),
+    i,
+    j;
 
-    var t = new twitter({
-        consumer_key: credentials.consumer_key,
-        consumer_secret: credentials.consumer_secret,
-        access_token_key: credentials.access_token_key,
-        access_token_secret: credentials.access_token_secret
-    });
+  t.stream(
+    'statuses/filter',
+    { track: trackedArray },
+    function(stream) {
+      stream.on("data", function(tweet) {
+        console.log(tweet.text);
 
-    t.stream(
-        'statuses/filter',
-        { track: ["awesome", "cool", "rad"] },
-        function(stream) {
-            stream.on("data", function(tweet) {
-                console.log(tweet.text);
-                //if awesome is in the tweet text, increment the counter                                                                                                                                                                        
-                if(tweet.text.indexOf("awesome") > -1) {
-                    client.incr("awesome");
-                }
-            });
-        }
-    );
+        for (i = 0; i < happyArray.length; i += 1) {
+          if(tweet.text.indexOf(happyArray[i]) > -1) {
+            client.incr("happy");
+          }
+        };
+        
+        for (j = 0; j < sadArray.length; j += 1) {
+          if(tweet.text.indexOf(sadArray[j]) > -1) {
+            client.incr("sad");
+          }
+        };
+      });
+    }
+  );
 };
 module.exports = worker;
